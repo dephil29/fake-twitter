e32d115164ebc74d5eb664e520f17da79c1b08ff
Eric Phillips <dephil29@hotmail.com>
2017-11-01 18:39:49 -0400

    moved data to different file, css, added more elements to function

--- server.js
+++ server.js
@@ -1,35 +1,16 @@
 const express = require("express");
 const app = express();
+const things = require("./things.js")
 
 //Set view engine to ejs-first
 app.set("view engine", "ejs");
 
-//index.page
-app.get("/", function(req, res){
-  const things = [
-  {
-    "created_at": "Thu Jun 22 21:00:00 +0000 2017",
-    "id": 1,
-    "text":
-      "Creating a Grocery List Manager Using Angular, Part 1: https://t.co/xFox78juL1",
-    "user": {
-      "id": 1,
-      "name": "SitePoint JavaScript",
-      "screen_name": "SitePointJS",
-      "location": "Melbourne, Australia",
-      "description": "Keep up with JavaScript tutorials, tips, tricks and articles at SitePoint.",
-      "followers_count": 2145,
-      "friends_count": 18,
-      "listed_count": 328,
-      "created_at": "Wed Aug 22 02:06:33 +0000 2012",
-      "favourites_count": 57,
-      "utc_offset": 43200,
-      "time_zone": "Wellington"
-    }
-  }
-]
+// console.log(things);
 
-  const tagLine = "Like the real one but greyer...";
+//index.page
+const tagLine = "Like the real one but greyer...";
+app.get("/", function(req, res){
+
 
   res.render("pages/index", {
     things: things,
---
+++ things.js
@@ -0,0 +1,64 @@
+const things = [
+  {
+    "created_at": "Thu Jun 22 21:00:00 +0000 2017",
+    "id": 1,
+    "text":
+      "Creating a Grocery List Manager Using Angular, Part 1: https://t.co/xFox78juL1",
+    "user": {
+      "id": 1,
+      "name": "SitePoint JavaScript",
+      "screen_name": "SitePointJS",
+      "location": "Melbourne, Australia",
+      "description": "Keep up with JavaScript tutorials, tips, tricks and articles at SitePoint.",
+      "followers_count": 2145,
+      "friends_count": 18,
+      "listed_count": 328,
+      "created_at": "Wed Aug 22 02:06:33 +0000 2012",
+      "favourites_count": 57,
+      "utc_offset": 43200,
+      "time_zone": "Wellington"
+    }
+  },
+  {
+    "created_at": "Thu Jun 22 21:00:00 +0000 2017",
+    "id": 2,
+    "text":
+      "To drink bleach or not to drink bleach. That is the question.",
+    "user": {
+      "id": 1,
+      "name": "SitePoint JavaScript",
+      "screen_name": "SitePointJS",
+      "location": "Melbourne, Australia",
+      "description": "Keep up with JavaScript tutorials, tips, tricks and articles at SitePoint.",
+      "followers_count": 2145,
+      "friends_count": 18,
+      "listed_count": 328,
+      "created_at": "Wed Aug 22 02:06:33 +0000 2012",
+      "favourites_count": 57,
+      "utc_offset": 43200,
+      "time_zone": "Wellington"
+    }
+  },
+  {
+    "created_at": "Thu Jun 22 21:00:00 +0000 2017",
+    "id": 3,
+    "text":
+      "Now I'm typing a bunch of stuff to see what it's going to look like when a bunch of stuff is typed into the box. What I'm hoping for is something that doesn't look too terrible. I wonder now if this is why twitter put a limit on the amount of characters one could input into a tweet. Seems like this should do it. If not I can always type more. Am I right or am I right? We'll see. But I'm right. Here we go. It's not quite as long as I wanted it to be. A curious thing happened when I tried to check it out though. I saved the file and refreshed the page but it wouldn't register the new class. It's like the function won't run until I reset the server but I thought that was the point. Maybe I didn't download nodemon. I'll check it out. I did download it I just didn't run it. For shame. So what was happening with my desktop at home. I'll have to figure out how that happened when I get home.",
+    "user": {
+      "id": 1,
+      "name": "SitePoint JavaScript",
+      "screen_name": "SitePointJS",
+      "location": "Melbourne, Australia",
+      "description": "Keep up with JavaScript tutorials, tips, tricks and articles at SitePoint.",
+      "followers_count": 2145,
+      "friends_count": 18,
+      "listed_count": 328,
+      "created_at": "Wed Aug 22 02:06:33 +0000 2012",
+      "favourites_count": 57,
+      "utc_offset": 43200,
+      "time_zone": "Wellington"
+    }
+  }
+]
+
+module.exports = things;
